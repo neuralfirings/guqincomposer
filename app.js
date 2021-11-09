5d6832a0126c0a03efee8e9a4b5ba882c7c5aa6b
nyl.io <hi@nyl.io>
2021-11-09 09:47:07 -0800

    initial code

---
+++ app.js
@@ -0,0 +1,332 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs')
+const { exec } = require('child_process');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json());
+
+var port = 7013;
+var server = app.listen(port, function () {
+  console.log(`HTTP listening on port ${port}.`);
+});
+
+app.use(express.static('public'))
+app.use('/nltabfiles', express.static('music'))
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+app.post('/renderly', function(req, res) {
+  console.log('/renderly', req.body.ly)
+  var ly = req.body.ly
+
+  var tempId = makeid(7)
+  fs.mkdirSync('./public/temp/' + tempId, { recursive: true });
+  fs.writeFileSync('./public/temp/' + tempId + '/music.ly', ly)
+  var bashScript = "lilypond -o ./public/temp/" + tempId + "/music --png --pdf -s ./public/temp/" + tempId + "/music.ly"
+
+  console.log(bashScript)
+  exec(bashScript, (err, stdout, stderr) => {
+    if (err) {
+      res.send({error: err, stdout: stdout, stderr: stderr})
+      return
+    }
+    files = fs.readdirSync('./public/temp/' + tempId)
+    pngs = []
+    for (var i=0;i<files.length;i++) {
+      if (files[i].indexOf('.png') > -1) 
+        pngs.push(files[i])
+    }
+    res.send({error: err, stdout: stdout, stderr: stderr, tempId: tempId, pngs: pngs})
+  })
+})
+
+app.get('/latest', function (req,res) {
+  console.log('/latest', req.query)
+  var id = req.query.id
+
+  if (fs.existsSync('./music/' + id)) {
+    var versions = fs.readdirSync('./music/' + id)
+    var version = 0
+    var imgurl = ''
+    for (var i=0;i<versions.length;i++) {
+      version = Math.max(version, Number(versions[i]))
+    }
+    if (fs.existsSync(`./music/${id}/${version}/music-page1.png`))
+      imgurl = `./nltabfiles/${id}/${version}/music-page1.png`
+    else
+      imgurl = `./nltabfiles/${id}/${version}/music.png`
+    gq = fs.readFileSync('./music/' + id + '/' + version + '/music.gq', 'utf-8')
+    // console.log(gq)
+    title = gq.split('title:')[1].split('\n')[0].trim()
+    console.log('version', version)
+    res.send({version: version, title: title, imgurl: imgurl, pdf: `./nltabfiles/${id}/${version}/music.pdf`})
+  }
+  else
+    res.send({error: 'invalid id'})
+})
+
+app.post('/save', function(req, res) {
+  console.log('/save', req.body.keepVersion)
+  var id = req.body.id
+  var oldId = req.body.id
+  var makenew = req.body.makenew == "true" ? true : false
+  var oldVersion = Number(req.body.version)
+  var shortHandVal = req.body.shortHandVal
+  var lilyPondVal = req.body.lilyPondVal
+  var hasFiles = req.body.hasFiles == "true" ? true : false
+  var tempId = req.body.tempId // temp music folder
+  var keepVersion = req.body.keepVersion == undefined ? 'no' : req.body.keepVersion 
+
+  var folder, version
+
+  if (makenew) {
+    console.log('make new')
+    id = makeid(7)
+    version = 1
+    folder = './music/' + id + '/' + version
+    fs.mkdirSync(folder, { recursive: true });
+  }
+  else {
+    console.log('no make new')
+    if (keepVersion == 'yes') {
+      version = oldVersion
+      folder = './music/' + id + '/' + version
+    }
+    else {
+      var versions = fs.readdirSync('./music/' + id)
+      version = 0
+      for (var i=0;i<versions.length;i++) {
+        version = Math.max(version, Number(versions[i]))
+      }
+      version++
+      folder = './music/' + id + '/' + version
+      fs.mkdirSync(folder, { recursive: true });
+    }
+  }
+  fs.writeFileSync(folder + '/music.gq', shortHandVal)
+  fs.writeFileSync(folder + '/music.ly', lilyPondVal)
+  if (tempId != undefined) {
+    var tempFiles = fs.readdirSync('./public/temp/' + tempId)
+    for (var i=0;i<tempFiles.length;i++) {
+      fs.copyFileSync('./public/temp/' + tempId + '/' + tempFiles[i], folder + '/' + tempFiles[i])
+    }
+  }
+  else if (hasFiles && keepVersion == 'no') {
+    var oldFiles = fs.readdirSync(`./music/${oldId}/${oldVersion}`)
+    for (var i=0;i<oldFiles.length;i++) {
+      if (oldFiles[i].indexOf('.pdf') > -1 || oldFiles[i].indexOf('.midi') > -1 || oldFiles[i].indexOf('.png') > -1)
+        fs.copyFileSync(`./music/${oldId}/${oldVersion}/${oldFiles[i]}`, folder + '/' + oldFiles[i])
+    }
+  }
+  res.send({ id: id, version: version})
+})
+
+app.get('/shorthand/:id/:version', function (req, res) {
+  var id = req.params.id 
+  var version = req.params.version
+
+  if (fs.existsSync('./music/' + id + "/" + version + "/music.gq")) {
+    var gq = fs.readFileSync('./music/' + id + "/" + version + "/music.gq", "utf-8")
+    res.send({shorthand: gq})
+  }
+  else {
+    res.send({error: 'no file found'})
+  }
+})
+
+app.get('/shorthand/:id', function (req, res) {
+  var id = req.params.id 
+  var versions = fs.readdirSync('./music/' + id)
+  var version = 0
+  for (var i=0;i<versions.length;i++) {
+    version = Math.max(version, Number(versions[i]))
+  }
+
+  if (fs.existsSync('./music/' + id + "/" + version + "/music.gq")) {
+    var gq = fs.readFileSync('./music/' + id + "/" + version + "/music.gq", "utf-8")
+    res.send({shorthand: gq, version: version})
+  }
+  else {
+    res.send({error: 'no file found'})
+  }
+})
+
+app.get('/nltabs', function(req,res) {
+  console.log('/nltabs')
+  var session = {
+    id: "new",
+    version: 0,
+    shortHandVal: `title: Lorem Ipsum\ntuning: g, a, c d e g a\n\nn: g,4. a,8 c2 | cc'2. g,4 $ | g4 g' e' d'8 c' | cc'4 C'8 G C'2\nf: 1 2 3 | by35v~~ 1j $ | 6 6v - - wc | /35bys v3u 1di vu3`,
+    lilyPondVal: "",
+    hasPDF: false,
+    hasMIDI: false,
+    hasPNG: false
+  }
+
+  var index = fs.readFileSync('./nltabs.html', 'utf-8')
+  index = index.split('{{variables}}').join("session = " + JSON.stringify(session))
+  res.send(index)
+})
+
+app.get('/nltabs/:id', function(req,res) {
+  var id = req.params.id
+  var versions = fs.readdirSync('./music/' + id)
+  var version = 0
+  for (var i=0;i<versions.length;i++) {
+    version = Math.max(version, Number(versions[i]))
+  }
+
+  var session = {
+    id: id,
+    version: version,
+    shortHandVal: fs.readFileSync('./music/' + id + "/" + version + "/music.gq", "utf-8"),
+    lilyPondVal: fs.readFileSync('./music/' + id + "/" + version + "/music.ly", "utf-8"),
+    hasPDF: false,
+    hasMIDI: false,
+    hasPNG: false,
+    PNGs: []
+  }
+  var files = fs.readdirSync('./music/' + id + '/' + version)
+  for (var i=0;i<files.length;i++) {
+    if (files[i].indexOf('.pdf') > -1)
+      session.hasPDF = true
+    if (files[i].indexOf('.midi') > -1)
+      session.hasMIDI = true
+    if (files[i].indexOf('.png') > -1) {
+      session.hasPNG = true
+      session.PNGs.push(files[i])
+    }
+  }
+
+  var index = fs.readFileSync('./nltabs.html', 'utf-8')
+  index = index.split('{{variables}}').join("session = " + JSON.stringify(session))
+  res.send(index)
+})
+
+app.get('/nltabs/:id/:version', function(req,res) {
+  var id = req.params.id
+  var version = req.params.version
+  var session = {
+    id: id,
+    version: version,
+    shortHandVal: fs.readFileSync('./music/' + id + "/" + version + "/music.gq", "utf-8"),
+    lilyPondVal: fs.readFileSync('./music/' + id + "/" + version + "/music.ly", "utf-8"),
+    hasPDF: false,
+    hasMIDI: false,
+    hasPNG: false,
+    PNGs: []
+  }
+  var files = fs.readdirSync('./music/' + id + '/' + version)
+  for (var i=0;i<files.length;i++) {
+    if (files[i].indexOf('.pdf') > -1)
+      session.hasPDF = true
+    if (files[i].indexOf('.midi') > -1)
+      session.hasMIDI = true
+    if (files[i].indexOf('.png') > -1) {
+      session.hasPNG = true
+      session.PNGs.push(files[i])
+    }
+  }
+
+  var index = fs.readFileSync('./nltabs.html', 'utf-8')
+  index = index.split('{{variables}}').join("session = " + JSON.stringify(session))
+  res.send(index)
+})
+
+app.post('/password/exists', function(req,res) {
+  console.log('/password/exists', req.body)
+  var id = req.body.id
+
+  if (!fs.existsSync('./.passwords')) {
+    fs.writeFileSync('./.passwords', '{}')
+    res.send({status: 'no'})
+  }
+  else {
+    var passwords = JSON.parse(fs.readFileSync('./.passwords', 'utf-8'))
+    if (passwords[id] == undefined) 
+      res.send({status: 'no'})
+    else
+      res.send({status: 'yes'})
+  }
+})
+
+app.post('/password/set', function(req,res) {
+  var id = req.body.id
+  var newPW = req.body.newPW 
+  var oldPW = req.body.oldPW
+
+  if (!fs.existsSync('./.passwords')) {
+    fs.writeFileSync('./.passwords', '{}')
+  }
+
+  var passwords = JSON.parse(fs.readFileSync('./.passwords', 'utf-8'))
+  if (passwords[id] == undefined) {
+    passwords[id] = newPW
+    fs.writeFileSync('./.passwords', JSON.stringify(passwords))
+    res.send({success: 'password set'})
+  }
+  else {
+    if (passwords[id] == oldPW) {
+      passwords[id] = newPW
+      fs.writeFileSync('./.passwords', JSON.stringify(passwords))
+      res.send({success: 'password set'})
+    }
+    else {
+      res.send({error: 'current password incorrect'})
+    }
+  }
+})
+
+
+app.post('/password/unset', function(req,res) {
+  var id = req.body.id
+  var pw = req.body.pw
+
+  var passwords = JSON.parse(fs.readFileSync('./.passwords', 'utf-8'))
+  if (passwords[id] == pw) {
+    passwords[id] = undefined
+    fs.writeFileSync('./.passwords', JSON.stringify(passwords))
+    res.send({success: 'password unset'})
+  }
+  else {
+    res.send({error: 'current password incorrect'})
+  }
+})
+
+app.post('/password/check', function(req,res) {
+  var id = req.body.id
+  var pw = req.body.pw 
+
+  var passwords = JSON.parse(fs.readFileSync('./.passwords', 'utf-8'))
+  if (passwords[id] == pw) {
+    res.send({success: 'valid'})
+  }
+  else {
+    res.send({error: 'current password incorrect'})
+  }
+})
+
+app.get('/test', function (req, res) {
+  res.send('hello world: ' + JSON.stringify(req.query) + '!');
+})
+
+app.get('/test/:id', function (req, res) {
+  res.send('id: ' + req.params.id)
+})
+
+app.post('/test', function (req, res) {
+  res.send('hello world: ' + JSON.stringify(req.body) + '!');
+})
+
+
---
+++ public/utils.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+  var nav = `
+      <a class="navbar-brand" href="/"><i class="fas fa-sliders-h"></i> Guqin Tablature Composer</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link" href="/nltabs/">New Score</a>
+          <a class="nav-item nav-link" href="/library.html">Library</a>
+          <a class="nav-item nav-link" href="/gettingstarted.html">Getting Started</a>
+          <a class="nav-item nav-link" href="/help.html">Docs</a>
+          <span class="show-on-score nav-item navbar-text">&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+          <a class="show-on-score save save-new-version nav-item nav-link" href="javascript:void(0)">Save<span class="unsaved">*</span></a>
+          <a class="show-on-score save save-keep-version nav-item nav-link" data-keep-version="yes" href="javascript:void(0)">Save (this version)</a>
+          <a class="show-on-score save-new nav-item nav-link" href="javascript:void(0)">Save As New</a>
+          <a class="show-on-score lock nav-item nav-link" href="javascript:void(0)">Set Password to Save</a>
+          <a class="show-on-score unlock nav-item nav-link" href="javascript:void(0)">Unset PW</a>
+        </div>
+      </div>`
+  $('.nav').addClass('navbar navbar-expand-lg navbar-dark bg-dark header').html(nav)
+  if (window.location.pathname.substr(0, 7) == '/nltabs') {
+  	$('.show-on-score').show()
+  }
+
+	$('.markdown').each(function() {
+		var md = $(this).html()
+		var mdLines = md.split('\n')
+		// console.log(mdLines.join('\n'))
+		var ws = ''
+		var i=0
+		do {
+			if (mdLines[i] != '')
+				ws = mdLines[i].split(mdLines[i].trim())[0]
+			i++
+		} while (i < mdLines.length && ws == '')
+		
+		for (var i=0;i<mdLines.length;i++) {
+			if (mdLines[i].substr(0, ws.length) == ws) {
+				mdLines[i] = mdLines[i].substr(ws.length)
+			}
+		}
+		// console.log(mdLines.join('\n'))
+    marked.setOptions({
+      gfm: true
+    })
+		$(this).html(marked.parse(mdLines.join('\n')))
+
+    $(this).find('table').each(function() {
+      $(this).addClass('table table-sm')
+    })
+	})
+
+  $('.gq-snippet').each(function() {
+  	var thisDiv = $(this)
+  	var id = $(this).attr('data-id') 
+  	var version = $(this).attr('data-version') 
+  	$.ajax({
+  		url: 'shorthand/' + id,
+  		success: function(output) {
+  			if (output.error) {
+  				console.log(output.error)
+  				return
+  			}
+  			
+  			console.log(output.shorthand)
+  			thisDiv.html(`
+					<div class="row">
+						<div class="col" style="width:50%">
+							<h5>Shorthand</h5>
+							<pre class="pre-ws-fix">` + output.shorthand + `</pre>
+						</div>
+						<div class="col" style="width:50%">
+							<h5>Score Render</h5>
+							<img src="https://guqin.nyl.io/nltabfiles/${id}/${output.version}/music.png" class="gq-snippet-img" />
+						</div>
+					</div>
+  			`)
+  			if (thisDiv.attr('data-img-height') != undefined) {
+  				thisDiv.find('.gq-snippet-img').css('height', thisDiv.attr('data-img-height') + 'px')
+  			}
+  			
+  		}
+  	})
+  })
+})
