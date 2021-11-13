bb603f3dc1a6a5894d76d9527166ac132175cafb
Nancy Yi Liang <nyl@NeuralFirings.local>
2021-11-13 07:57:56 -0800

    https://github.com/neuralfirings/guqincomposer/issues/2

--- public/guqintranslator.js
+++ public/guqintranslator.js
@@ -28,7 +28,7 @@
   const lyStr1 = "% v9\nlm = #(define-music-function (note) (ly:music?) #{  \\override NoteHead.style = #'cross $note \\revert NoteHead.style #} )\nfy = #(define-music-function (pos note) ((number? 1) ly:music?) (let ( (stringNum (ly:prob-property (list-ref (ly:music-property note 'articulations) 0) 'string-number)) (tuning "
   const lyStr2 = ") (let ( (base (list-ref tuning stringNum)) ) (define noteP (ly:music-property note 'pitch)) (define baseP (ly:music-property base 'pitch)) (define intNote (ly:pitch-diff (ly:music-property base 'pitch) (ly:music-property note 'pitch) )) (define interval (ly:pitch-tones (ly:pitch-diff (ly:music-property note 'pitch) (ly:music-property base 'pitch)))) (cond ( (= interval 6 ) #{ \\transpose c f, { \\harmonicByFret #7 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 9.5 ) (= pos 5)) #{ \\transpose c g, { \\harmonicByFret #5 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 9.5 ) (= pos 9)) #{ \\transpose c ees, { \\harmonicByFret #9 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 9.5 ) (= pos 1)) #{ \\transpose c g, { \\harmonicByFret #5 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 12 ) (= pos 4)) #{ \\transpose c aes, { \\harmonicByFret #4 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 12 ) (= pos 10)) #{ \\transpose c c'' { \\harmonicByFret #10 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 12 ) (= pos 1)) #{ \\transpose c aes, { \\harmonicByFret #4 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 14 ) (= pos 3)) #{ \\transpose c a, { \\harmonicByFret #3 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 14 ) (= pos 6)) #{ \\transpose c e'' { \\harmonicByFret #6 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 14 ) (= pos 8)) #{ \\transpose c e'' { \\harmonicByFret #8 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 14 ) (= pos 11)) #{ \\transpose c e'' { \\harmonicByFret #11 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 14 ) (= pos 1)) #{ \\transpose c e'' { \\harmonicByFret #6 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 15.5 ) (= pos 2)) #{ \\transpose c f, { \\harmonicByFret #2 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 15.5 ) (= pos 12)) #{ \\transpose c g' { \\harmonicByFret #12 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 15.5 ) (= pos 1)) #{ \\transpose c f, { \\harmonicByFret #2 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 17 ) (= pos 1.4)) #{ \\transpose c bes'' { \\harmonicByFret #1.4 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 17 ) (= pos 4.4)) #{ \\transpose c bes'' { \\harmonicByFret #4.4 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 17 ) (= pos 6.3)) #{ \\transpose c bes'' { \\harmonicByFret #6.3 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 17 ) (= pos 7.7)) #{ \\transpose c bes'' { \\harmonicByFret #7.7 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 17 ) (= pos 9.6)) #{ \\transpose c bes'' { \\harmonicByFret #9.6 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 17 ) (= pos 12.6)) #{ \\transpose c bes'' { \\harmonicByFret #12.6 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 17 ) (= pos 1)) #{ \\transpose c bes'' { \\harmonicByFret #6.3 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 18 ) (= pos 1)) #{ \\transpose c c''' { \\harmonicByFret #1 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 18 ) (= pos 5.6)) #{ \\transpose c c''' { \\harmonicByFret #5.6 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 18 ) (= pos 8.4)) #{ \\transpose c c''' { \\harmonicByFret #8.4 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 18 ) (= pos 13)) #{ \\transpose c c''' { \\harmonicByFret #13 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 19 ) (= pos 0.9)) #{ \\transpose c d''' { \\harmonicByFret #0.9 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 19 ) (= pos 3.4)) #{ \\transpose c d''' { \\harmonicByFret #3.4 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 19 ) (= pos 6.4)) #{ \\transpose c d''' { \\harmonicByFret #6.4 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 19 ) (= pos 7.6)) #{ \\transpose c d''' { \\harmonicByFret #7.6 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 19 ) (= pos 10.6)) #{ \\transpose c d''' { \\harmonicByFret #10.6 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 19 ) (= pos 13.1)) #{ \\transpose c d''' { \\harmonicByFret #13.1 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 19 ) (= pos 1)) #{ \\transpose c d''' { \\harmonicByFret #6.4 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 20 ) (= pos 0.8)) #{ \\transpose c e''' { \\harmonicByFret #0.8 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 20 ) (= pos 4.6)) #{ \\transpose c e''' { \\harmonicByFret #4.6 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 20 ) (= pos 9.4)) #{ \\transpose c e''' { \\harmonicByFret #9.4 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 20 ) (= pos 13.2)) #{ \\transpose c e''' { \\harmonicByFret #13.2 $(ly:music-transpose note intNote) } #} ) ( (and (= interval 20 ) (= pos 1)) #{ \\transpose c e''' { \\harmonicByFret #4.6 $(ly:music-transpose note intNote) } #} ) ( else #{ $note #} ) ) ) ) )"
   const lyStr3 = `tuningEqual = #'("0" "13.6"  "13.1"  "12.2"  "10.9"  "10"  "9.5"  "9"  "8.4"  "7.9"  "7.6"  "7.3"  "7"  "6.7"  "6.5"  "6.2"  "6"  "5.6"  "5.3"  "5"  "4.8"  "4.6"  "4.4"  "4.2"  "4"  "3.7"  "3.5"  "3.2"  "3"  "2.6"  "2.3"  "2"  "1.8"  "1.6"  "1.4"  "1.2"  "1")\ntuningJust = #'("0" "13.6" "13.1" "12.2" "11" "10" "9.5" "9" "8.5" "8" "7.7" "7.3" "7" "6.7" "6.4" "6.2" "6" "5.6" "5.3" "5" "4.8" "4.6" "4.4" "4.2" "4" "3.7" "3.4" "3.2" "3" "2.6" "2.3" "2" "1.8" "1.6" "1.4" "1.2" "1")`
-  const lyStr4 = `\\layout { \\context { \\TabStaff stringTunings = \\stringTuning \\tuning fretLabels = \\tuningEqual }`
+  const lyStr4 = `\\layout { \\context { \\TabStaff stringTunings = \\stringTuning \\tuning fretLabels = ` //\\tuningEqual }`
   const lyStr5 = `\\context { \\Score \\override RehearsalMark.self-alignment-X = #LEFT \\override RehearsalMark.font-size = #-1 } \\context { \\Staff \\hide TextScript \\override TrillSpanner.bound-details.left.text = ##f \\override Glissando.style = #'zigzag }`
   const lyStr6 = `\\context { \\TabStaff \\omit Clef \\omit ClefModifier \\revert TextScript.stencil \\override TextScript.font-size = #-3 \\override Glissando.style = #'zigzag \\override TabNoteHead.font-family = #'typewriter tablatureFormat = #fret-letter-tablature-format } \\textLengthOn \\omit Voice.StringNumber } \\midi {}`
   const lyStr7 = `<< \\new Staff  { \\song } \\new TabStaff { \\clef "moderntab" \\song } >>`
@@ -175,6 +175,7 @@ function getPrevNote(song, i) {
 function shortHandToGuqinJSON(shortHand) {
   var shortHandLines = shortHand.split('\n')
   var tuning=''
+  var intonation='\\tuningEqual'
   var composer='' 
   var title=''
   var endnote=''
@@ -195,6 +196,9 @@ function shortHandToGuqinJSON(shortHand) {
         if (shortHandLines[i].beginsWith("tuning:")) {
           var tuningStr = shortHandLines[i].split('tuning:')[1]
           tuning = tuningStr.trim().split(' ')
+        }
+        if (shortHandLines[i].beginsWith("intonation:")) {
+          var intonation = shortHandLines[i].split('intonation:')[1].trim() == 'just' ? '\\tuningJust' : '\\tuningEqual'
         }
         else if (shortHandLines[i].beginsWith('showtimesig:')) {
           showtimesig = shortHandLines[i].split('showtimesig:')[1].trim()
@@ -295,15 +299,18 @@ function shortHandToGuqinJSON(shortHand) {
   // { error check the song
     for (var i=0; i<song.length; i++) {
       if (song[i].beginsWith('|:') && song[i] != '|:|') {
-        alert(`Oops! Something looks fishy on line ${songLineIdx[i]}. There's a bar mistmatch between note line and finger line.`)
+        console.log('error')
+        alert(`Error on Line ${songLineIdx[i]}.\n\nThere's a mismatch between the number of notes in the note (n:) and finger positions (f:) lines.`)
         return false
       }
       else if (song[i].beginsWith('||:') && song[i] != '||:||') {
-        alert(`Oops! Something looks fishy on line ${songLineIdx[i]}. There's a bar mistmatch between note line and finger line.`)
+        console.log('error')
+        alert(`Error on Line ${songLineIdx[i]}.\n\nThere's a mismatch between the number of notes in the note (n:) and finger positions (f:) lines.`)
         return false
       }
       else if (song[i].beginsWith('$:') && song[i] != '$:$') {
-        alert(`Oops! Something looks fishy on line ${songLineIdx[i]}. There's a glissando ($) mistmatch between note line and finger line.`)
+        console.log('error')
+        alert(`Error on Line ${songLineIdx[i]}.\n\nThere's a mismatch between the number of notes in the note (n:) and finger positions (f:) lines around the glissando ($).`)
         return false
       }
 
@@ -332,7 +339,8 @@ function shortHandToGuqinJSON(shortHand) {
         }
       }
       if (!validNote) {
-        alert(`Oops! Something looks fishy on line ${songLineIdx[i]} (${song[i]}). Looks like an invalid entry in the note line.`)
+        console.log('error')
+        alert(`Error on Line${songLineIdx[i]}.\n\nLooks like an invalid entry in the note line (n:): ${notePart}`)
         return false
       }
     }
@@ -572,6 +580,7 @@ function shortHandToGuqinJSON(shortHand) {
 
   return {
     tuning: tuning,
+    intonation: intonation,
     title: title,
     composer: composer,
     endnote: endnote,
@@ -672,7 +681,7 @@ function guqinToLilyPond(guqinJSON) {
         ly += "\\clef \"" + song[i].value + "\"\n  "
       }
       else if (song[i].type == 'mark') {
-        ly += '\\mark \\markup \\left-column {"' + song[i].value.split('\\n').join('" "') + '" }\n'
+        ly += '\\mark \\markup \\left-column {"' + song[i].value.split('\\n').join('" "') + '" }\n  '
       }
       else if (song[i].type == 'lilypond') {
         ly += song[i].value + "\n  "
@@ -853,7 +862,7 @@ function guqinToLilyPond(guqinJSON) {
 
   // { Tuning, score layouts
     ly +=lyStr3 + '\n\n'
-    ly += `\\score {\n  ` + lyStr4 + '\n'
+    ly += `\\score {\n  ` + lyStr4 + guqin.intonation + ' }\n'
     if (guqin.showtimesig == 'no') {
       ly += `  \\context { \\Staff \\omit TimeSignature }\n`
     }
