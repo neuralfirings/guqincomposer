3b76e012f5151b8b7ec0f9a19a4709bc32a50b34
Nancy Yi Liang <hi@nyl.io>
2021-11-16 22:40:08 -0800

    bugfix

--- public/mode/guqinshorthand.js
+++ public/mode/guqinshorthand.js
@@ -17,7 +17,8 @@ CodeMirror.defineSimpleMode("guqinjzp", {
     {regex: /D|F|S|V|c|d|f|g|h|i|j|k|l|n|o|s|u|v|w|x|by|yb|b8|8b/, token: "jzp"},
     {regex: / \| | \. | \|\| | \.\| | \.\. | \|\.\| | \|. | \.\|\: | \:\.\.\: | \:\|\.\|\: | \:\.\|\.\: | \[\|\: | \:\|\]\[\|\: | \:\|\] | \:\|\. /, token: "bars"},
     {regex: / \|$| \.$| \|\|$| \.\|$| \.\.$| \|\.\|$| \|.$| \.\|\:$| \:\.\.\:$| \:\|\.\|\:$| \:\.\|\.\:$| \[\|\:$| \:\|\]\[\|\:$| \:\|\]$| \:\|\./, token: "bars"},
-    {regex: /(?:function|var|return|if|for|while|else|do|this)\b/, token: "keyword"},
+    // {regex: /(?:function|var|return|if|for|while|else|do|this)\b/, token: "keyword"},
+    {regex: /\?/, token: "uncertainty"}
   ]
 });
 
